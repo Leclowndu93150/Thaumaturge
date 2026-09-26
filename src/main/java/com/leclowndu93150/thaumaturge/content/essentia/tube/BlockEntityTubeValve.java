d3f7205fcb75c6cf5ae33b349729ff04064449a1
Saereth <guiken@gmail.com>
2026-09-26 15:09:01 -0700

    Fix essentia flow through closed valves

--- src/main/java/com/leclowndu93150/thaumaturge/content/essentia/tube/BlockEntityTubeValve.java
+++ src/main/java/com/leclowndu93150/thaumaturge/content/essentia/tube/BlockEntityTubeValve.java
@@ -34,6 +34,9 @@ public final class BlockEntityTubeValve extends BlockEntityTube {
 
     public void setAllowFlow(boolean allow) {
         this.allowFlow = allow;
+        if (!allow) {
+            super.setSuction(null, 0);
+        }
         setChanged();
         if (level != null && !level.isClientSide()) {
             level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), 3);
@@ -52,6 +55,7 @@ public final class BlockEntityTubeValve extends BlockEntityTube {
             }
             if (!wasPoweredLastTick && powered && allowFlow) {
                 allowFlow = false;
+                super.setSuction(null, 0);
                 level.playSound(null, pos, TCSounds.SQUEEK.get(), SoundSource.BLOCKS, 0.7F, 0.9F + level.getRandom().nextFloat() * 0.2F);
                 setChanged();
                 level.sendBlockUpdated(pos, state, state, 3);
@@ -80,10 +84,34 @@ public final class BlockEntityTubeValve extends BlockEntityTube {
     }
 
     @Override
+    public int addEssentia(Holder<IAspect> aspect, int amount, Direction face) {
+        if (!allowFlow)
+            return 0;
+        return super.addEssentia(aspect, amount, face);
+    }
+
+    @Override
+    public int takeEssentia(Holder<IAspect> aspect, int amount, Direction face) {
+        if (!allowFlow)
+            return 0;
+        return super.takeEssentia(aspect, amount, face);
+    }
+
+    @Override
     public void setSuction(Holder<IAspect> aspect, int amount) {
-        if (allowFlow) {
+        if (allowFlow || amount <= 0) {
             super.setSuction(aspect, amount);
         }
+    }
+
+    @Override
+    public Holder<IAspect> getSuctionType(Direction face) {
+        return allowFlow ? super.getSuctionType(face) : null;
+    }
+
+    @Override
+    public int getSuctionAmount(Direction face) {
+        return allowFlow ? super.getSuctionAmount(face) : 0;
     }
 
     @Override
